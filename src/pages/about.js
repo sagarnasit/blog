d2fca878611628e070ea3d20c6e3d5e6fb82eadb
sagarnasit <sagarnasit@gmail.com>
2019-07-06 17:43:49 +0530

    Create layout and global css for page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../componenets/layout';
+
+export default () => (
+  <Layout>
+    <h1>About Page</h1>
+
+    <Link to="/">&larr; Back to home</Link>
+  </Layout>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../componenets/layout';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => <Layout>Hello Frontend Masters!</Layout>;
