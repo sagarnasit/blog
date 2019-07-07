e86ea2c097b49b22f5a5bf23c5463613cfe302f5
sagarnasit <sagarnasit@gmail.com>
2019-07-07 16:12:20 +0530

    Set gatsby node to generate posts from post directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
 
-export default () => <Layout>Hello Frontend Masters!</Layout>;
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
